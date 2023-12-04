d9b2d11b19b1387e7fc14d125590bb3aff986ea5
Krishnaraj S <krishnarajshetty699@gmail.com>
2023-12-04 21:20:57 +0530

    Day 2

---
+++ Java/containsDuplicate.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> hash=new HashSet<>();
+        for(int num:nums){
+            if(hash.contains(num)){
+                return true;
+            }
+            else{
+                hash.add(num);
+            }    
+        }
+        return false;
+    }
+}
